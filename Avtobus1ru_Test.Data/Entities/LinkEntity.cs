3f7992751b2d1ce5a2d83844a2fa61b08f4a87e5
neerayel <andrei11shum@mail.ru>
2026-02-12 22:46:06 +0300

    EF entity change

--- Avtobus1ru_Test.Data/Entities/LinkEntity.cs
+++ Avtobus1ru_Test.Data/Entities/LinkEntity.cs
@@ -5,9 +5,9 @@ namespace Avtobus1ru_Test.Data.Entities
     public class LinkEntity
     {
         [Key]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string LongURL { get; set; }
-        public string ShortURL { get; set; }
+        public string ShortURLKey { get; set; }
         public DateTime CreationDate { get; set; }
         public int ClickCount { get; set; }
     }
