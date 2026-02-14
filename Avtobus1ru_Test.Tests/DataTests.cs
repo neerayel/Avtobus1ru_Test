76949b0162c7a90e0d194105ef6a187777cc5c48
neerayel <andrei11shum@mail.ru>
2026-02-14 20:58:30 +0300

    Add EF repository tests

--- Avtobus1ru_Test.Tests/DataTests.cs
+++ Avtobus1ru_Test.Tests/DataTests.cs
@@ -1,61 +1,122 @@
-﻿using Avtobus1ru_Test.Data.Entities;
+﻿using Avtobus1ru_Test.Data;
+using Avtobus1ru_Test.Data.Entities;
+using Avtobus1ru_Test.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Avtobus1ru_Test.Tests
 {
     public class DataTests
     {
+        static LinkEntity emptyEntity = new LinkEntity()
+        {
+            Id = 1,
+            LongURL = "",
+            ShortURLKey = "",
+            CreationDate = new DateTime(2026, 2, 14, 12, 0, 0),
+            ClickCount = 0
+        };
+
         static LinkEntity testEntity = new LinkEntity()
         {
-            Id = 0,
+            Id = 1,
             LongURL = "https://www.fake-url.com",
             ShortURLKey = "ab593c12-0188-400f-8764-261e9d3efd35",
-            CreationDate = new DateTime(2026, 2, 13, 12, 0, 0),
+            CreationDate = new DateTime(2026, 2, 14, 12, 0, 0),
             ClickCount = 1
         };
 
         static List<LinkEntity> linkEntities = new List<LinkEntity> { testEntity };
 
 
+        private DataContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
+            return new DataContext(optionsBuilder.Options);
+        }
+
+        private LinkRepository GetRepository(DataContext context)
+        {
+            return new LinkRepository(context);
+        }
+
+
         [Fact]
         public async Task CreateTest()
         {
-            Assert.True(false);
+            var context = GetDbContext();
+            var repository = GetRepository(context);
+
+            await repository.CreateAsync(testEntity);
+
+            Assert.Single(context.Links);
         }
 
         [Fact]
         public async Task GetAllTest()
         {
-            Assert.True(false);
+            var repository = GetRepository(GetDbContext());
+
+            await repository.CreateAsync(testEntity);
+            var result = await repository.GetAllAsync();
+
+            Assert.Equal(result, linkEntities);
         }
 
         [Fact]
         public async Task GetByIdTest()
         {
-            Assert.True(false);
+            var repository = GetRepository(GetDbContext());
+
+            await repository.CreateAsync(testEntity);
+            var result = await repository.GetByIdAsync(testEntity.Id);
+
+            Assert.Equal(result, testEntity);
         }
 
         [Fact]
         public async Task GetLongFromShortTest()
         {
-            Assert.True(false);
+            var repository = GetRepository(GetDbContext());
+
+            await repository.CreateAsync(testEntity);
+            var result = await repository.GetLongFromShortAsync(testEntity.ShortURLKey);
+
+            Assert.Equal(result, testEntity);
         }
 
         [Fact]
         public async Task GetShortFromLongTest()
         {
-            Assert.True(false);
+            var repository = GetRepository(GetDbContext());
+
+            await repository.CreateAsync(testEntity);
+            var result = await repository.GetShortFromLongAsync(testEntity.LongURL);
+
+            Assert.Equal(result, linkEntities);
         }
 
         [Fact]
         public async Task UpdateTest()
         {
-            Assert.True(false);
+            var repository = GetRepository(GetDbContext());
+
+            await repository.CreateAsync(emptyEntity);
+            await repository.UpdateAsync(testEntity);
+            var result = await repository.GetByIdAsync(testEntity.Id);
+
+            Assert.Equal(result, testEntity);
         }
 
         [Fact]
         public async Task DeleteTest()
         {
-            Assert.True(false);
+            var context = GetDbContext();
+            var repository = GetRepository(context);
+
+            await repository.CreateAsync(testEntity);
+            await repository.DeleteAsync(testEntity.Id);
+
+            Assert.Empty(context.Links);
         }
     }
 }
